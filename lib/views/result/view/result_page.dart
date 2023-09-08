704e30b86171c41634b030265aea9cddcd0f0070
Gaurav Katva <gaurav@grappus.com>
2023-09-08 23:18:27 +0530

    feat: animtion added

--- lib/views/result/view/result_page.dart
+++ lib/views/result/view/result_page.dart
@@ -3,76 +3,119 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:grappus_skribbl/views/views.dart';
+import 'package:lottie/lottie.dart';
 import 'package:models/models.dart';
 
-class ResultPage extends StatelessWidget {
+class ResultPage extends StatefulWidget {
   const ResultPage({super.key});
 
   @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> with TickerProviderStateMixin {
+  final celebrationAnimation = Assets.celebrationAnimtaion;
+  late final AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Future.delayed(
+          const Duration(milliseconds: 500),
+        ).then((value) => Navigator.pop(context));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BaseBackground(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Game Results',
-              style: context.textTheme.bodyLarge?.copyWith(
-                fontFamily: paytoneOne,
-                color: AppColors.pastelPink,
-                fontSize: 40,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'The winners are',
-              style: context.textTheme.bodyLarge?.copyWith(
-                color: AppColors.white.withOpacity(0.7),
-                fontSize: 24,
-              ),
-            ),
-            const SizedBox(height: 50),
-            BlocBuilder<GameCubit, GameState>(
-              builder: (context, state) {
-                if (state.sessionState == null) return const SizedBox();
-                final leaderBoard = state.sessionState?.leaderboard;
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if ((leaderBoard?.length ?? 0) > 2)
-                      _ResultCard(
-                        player: leaderBoard![2],
-                        rank: 3,
-                      ),
-                    ...List.generate(
-                      (leaderBoard?.length ?? 0) > 2
-                          ? (leaderBoard?.length ?? 0) - 1
-                          : leaderBoard?.length ?? 0,
-                      (index) => _ResultCard(
-                        player: leaderBoard![index],
-                        rank: index + 1,
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-            const SizedBox(height: 50),
-            SkribblButton(
-              onTap: () {
-                context.read<GameCubit>().close();
-                Navigator.of(context).pushAndRemoveUntil<Widget>(
-                  MaterialPageRoute(
-                    builder: (context) => const OnboardingPage(),
-                  ),
-                  (_) => false,
-                );
-              },
-              text: 'Continue',
-            ),
-          ],
+    return Stack(
+      children: [
+        Lottie.asset(
+          celebrationAnimation,
+          repeat: false,
+          controller: _controller,
+          onLoaded: (composition) {
+            // Configure the AnimationController with the duration of the
+            // Lottie file and start the animation.
+            _controller
+              ..duration = composition.duration
+              ..forward();
+          },
         ),
-      ),
+        BaseBackground(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Game Results',
+                  style: context.textTheme.bodyLarge?.copyWith(
+                    fontFamily: paytoneOne,
+                    color: AppColors.pastelPink,
+                    fontSize: 40,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'The winners are',
+                  style: context.textTheme.bodyLarge?.copyWith(
+                    color: AppColors.white.withOpacity(0.7),
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                BlocBuilder<GameCubit, GameState>(
+                  builder: (context, state) {
+                    if (state.sessionState == null) return const SizedBox();
+                    final leaderBoard = state.sessionState?.leaderboard;
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        if ((leaderBoard?.length ?? 0) > 2)
+                          _ResultCard(
+                            player: leaderBoard![2],
+                            rank: 3,
+                          ),
+                        ...List.generate(
+                          (leaderBoard?.length ?? 0) > 2
+                              ? (leaderBoard?.length ?? 0) - 1
+                              : leaderBoard?.length ?? 0,
+                          (index) => _ResultCard(
+                            player: leaderBoard![index],
+                            rank: index + 1,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                const SizedBox(height: 50),
+                SkribblButton(
+                  onTap: () {
+                    context.read<GameCubit>().close();
+                    Navigator.of(context).pushAndRemoveUntil<Widget>(
+                      MaterialPageRoute(
+                        builder: (context) => const OnboardingPage(),
+                      ),
+                      (_) => false,
+                    );
+                  },
+                  text: 'Continue',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
