610603bcefcb5a679ae44d17f75ea80d69221d2c
mox <moksh@grappus.com>
2023-09-11 15:04:51 +0530

    fix:backend

--- packages/services/lib/src/endpoints.dart
+++ packages/services/lib/src/endpoints.dart
@@ -11,9 +11,9 @@ class Endpoints {
 
   static String localhostBaseUrl = 'http://localhost:8080';
   static String serverHostUrl =
-      'http://ec2-13-51-233-255.eu-north-1.compute.amazonaws.com';
+      'https://graptoons-api.rpsite.top/';
 
   static String localhostWebStockUrl = 'ws://localhost:8080/ws';
   static const String serverWebscoketUrl =
-      'ws://ec2-13-51-233-255.eu-north-1.compute.amazonaws.com/ws';
+      'ws://graptoons-api.rpsite.top/ws';
 }
